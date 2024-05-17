bba9437d0b4b5cd3154adae4ea5a17b88fea1822
ravigupta <rg187888@gmail.com>
2024-05-17 11:44:29 +0530

    data modeling in mongoose

---
+++ mongoose_part-1/models/todo.models.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true });
+
+export const Todo = mongoose.model("Todo", todoSchema);
