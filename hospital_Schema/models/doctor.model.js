0829e8cfd591a50c95a25d012dca428cb164a477
ravigupta <rg187888@gmail.com>
2024-05-18 22:22:17 +0530

    add details in models

--- hospital_Schema/models/doctor.model.js
+++ hospital_Schema/models/doctor.model.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, {timestamps: true});
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    },
+    qualification: {
+        type: String,
+        required: true
+    },
+    experienceInYear: {
+        type: Number,
+        default: 0
+    },
+    worksInHospital: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hospital'
+        }
+    ]
+}, {timestamps: true});
 
 export const Doctor = mongoose.model('Doctor', doctorSchema)
--- hospital_Schema/models/medical_record.model.js
+++ hospital_Schema/models/medical_record.model.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema({
+    recordName:{
+        type: String,
+        required: true
+    },
+    duration:{
+        type: String,
+        default: 0,
+        required: true
+    }
+}, { timestamps: true });
 
 export const MedicalRecord = mongoose.model('MedicalRecord', medicalRecordSchema)
